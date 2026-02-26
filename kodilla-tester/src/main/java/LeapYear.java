cc3b549540975df7a25ecaa9b25e940f787016f3
kacpermochniej <kacper.mochniej@poczta.onet.pl>
2026-02-26 13:05:28 +0100

    Update LeapYear.java

--- kodilla-tester/src/main/java/LeapYear.java
+++ kodilla-tester/src/main/java/LeapYear.java
@@ -1,2 +1,19 @@
 public class LeapYear {
+
+    public static void main(String[] args) {
+        int yearToCheck = 2024;
+        boolean isLeap = isLeapYear(yearToCheck);
+        if (isLeap) {
+            System.out.println(yearToCheck + " jest rokiem przestępnym.");
+        } else {
+            System.out.println(yearToCheck + " nie jest rokiem przestępnym.");
+        }
+    }
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
