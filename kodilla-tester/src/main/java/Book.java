52282b29e2bae1208224d3070e05dfd1043f93fd
Kacper Mochniej <kacper.mochniej@poczta.onet.pl>
2026-02-26 13:40:21 +0100

    Initial commit

---
+++ kodilla-tester/src/main/java/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private String author;
+    private String title;
+
+
+    private Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+
+    public static Book of(String author, String title) {
+        return new Book(author, title);
+    }
+
+
+    public String toString() {
+        return "Book: " + title + " by " + author;
+    }
+
+    public static void main(String[] args) {
+
+        Book book = Book.of("Isaac Asimov", "The Galaxy");
+
+        System.out.println(book);
+    }
+}
